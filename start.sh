e339fdffac1a7195c043e420d477e12261eef116
YI-CHENG-SHIH645 <z2x98653322@gmail.com>
2022-12-18 06:30:13 +0800

    modify

---
+++ start.sh
@@ -0,0 +1,7 @@
+cd backend_api || exit
+sudo docker build . -t backend_api
+sudo docker run -p 3000:3000 -v "$PWD/public:/home/node/app/public" -d backend_api
+
+cd ../frontend || exit
+sudo docker build . -t frontend
+sudo docker run -p 3001:3001 -d frontend
