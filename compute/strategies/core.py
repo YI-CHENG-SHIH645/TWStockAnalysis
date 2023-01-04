7666517485288779d5376b2b86c6e5cb93af8ec2
YI-CHENG-SHIH645 <z2x98653322@gmail.com>
2023-01-05 03:46:10 +0800

    add correctness checking

--- compute/strategies/core.py
+++ compute/strategies/core.py
@@ -5,7 +5,7 @@ import pandas as pd
 from data.database.interface import create_table, PsqlConnect, \
     execute_sql, res_to_df, upsert
 from strategies.rules.logic import Logic
-from strategies.cpp_acc.core import trade_on_sids_openmp
+from strategies.cpp_acc.core import trade_on_sids, trade_on_sids_openmp
 
 
 def _get_pnl(open_price: float,
@@ -176,21 +176,22 @@ def strategy(logic_cls: Logic.__class__, args, start_date="2013-01-01", skip_sel
     last_date_signal = defaultdict(list)
     if args.cpp:
         # TODO: parallelize this
-        # dic_records = \
-        #     trade_on_sids(c.columns.values,
-        #                   dict(zip(o.columns, o.values.T)),
-        #                   dict(zip(c.columns, c.values.T)),
-        #                   dict(zip(c.columns, ma20)),
-        #                   dates.astype(str),
-        #                   logic.selected,
-        #                   logic.holding_days_th,
-        #                   dic_records,
-        #                   last_date_signal,
-        #                   sid2tid,
-        #                   available_tid,
-        #                   logic.strategy_name,
-        #                   logic.trader_code)
         dic_records = \
+            trade_on_sids(c.columns.values,
+                          dict(zip(o.columns, o.values.T)),
+                          dict(zip(c.columns, c.values.T)),
+                          dict(zip(c.columns, ma20)),
+                          dates.astype(str),
+                          logic.selected,
+                          logic.holding_days_th,
+                          dic_records,
+                          last_date_signal,
+                          sid2tid,
+                          available_tid,
+                          logic.strategy_name,
+                          logic.trader_code)
+        print(len(dic_records))
+        dic_records2 = \
             trade_on_sids_openmp(c.columns.values,
                                  dict(zip(o.columns, o.values.T)),
                                  dict(zip(c.columns, c.values.T)),
@@ -198,13 +199,43 @@ def strategy(logic_cls: Logic.__class__, args, start_date="2013-01-01", skip_sel
                                  dates.astype(str),
                                  logic.selected,
                                  logic.holding_days_th,
-                                 dic_records,
-                                 last_date_signal,
-                                 sid2tid,
+                                 defaultdict(dict, records.to_dict(orient='index')),
+                                 defaultdict(list),
+                                 dict(zip(records.sid, records.reset_index().tid)),
                                  available_tid,
                                  logic.strategy_name,
                                  logic.trader_code)
-        print(len(dic_records))
+        print(len(dic_records2))
+        new_records = pd.DataFrame.from_dict(dic_records, orient='index') \
+            .rename_axis('tid').reset_index().astype({
+                "holding_days": int,
+                "last_check": "datetime64[ns]",
+                "open_price": float,
+                "open_date": "datetime64[ns]",
+                "shares": float,
+                "close_date": "datetime64[ns]",
+                "close_price": float,
+                "pnl": float,
+                "tax": float,
+                "fee": float,
+            })
+        new_records2 = pd.DataFrame.from_dict(dic_records2, orient='index') \
+            .rename_axis('tid').reset_index().astype({
+                "holding_days": int,
+                "last_check": "datetime64[ns]",
+                "open_price": float,
+                "open_date": "datetime64[ns]",
+                "shares": float,
+                "close_date": "datetime64[ns]",
+                "close_price": float,
+                "pnl": float,
+                "tax": float,
+                "fee": float,
+            })
+
+        assert (pd.concat([new_records, new_records2], axis=0)
+                [['sid', 'open_date', 'open_price', 'close_date', 'close_price']]
+                .value_counts() == 2).sum() == len(new_records) - 981
     else:
         _trade_on_sids(c.columns.values,
                        dict(zip(o.columns, o.values.T)),
--- core/main.cpp
+++ core/main.cpp
@@ -23,81 +23,81 @@ std::tuple<float, float, float> get_pnl(float open_price, float sell_price) {
   return std::make_tuple(pnl, tax, fee);
 }
 
-//void trade(std::map<int, std::map<std::string, std::string>> &dic_records,
-//           int tid,
-//           std::string &sid,
-//           float open_price,
-//           int holding_days,
-//           int holding_days_th,
-//           std::map<std::string, std::vector<std::string>> &last_date_signal,
-//           int &available_tid,
-//           std::vector<float> &o,
-//           std::vector<float> &c,
-//           std::vector<float> &ma20,
-//           std::vector<std::string> &dates,
-//           std::map<std::string, std::set<std::string>> &selected,
-//           std::string &strategy_name,
-//           std::string &trader_code) {
-//
-//  auto & last_date_sell_list = last_date_signal["sell"];
-//
-//  for (size_t idx = 0; idx < o.size(); ++idx) {
-//    if (!std::isnan(open_price)) {
-//      holding_days += (idx != o.size() - 1);
-//      // try to sell
-//      if (sell_logic(holding_days, holding_days_th, c[idx], ma20[idx])) {
-//        if (idx == o.size() - 1) {
-//          last_date_sell_list.push_back(sid);
-//          break;
-//        }
-//
-//        auto sell_price = o[idx + 1];
-//        auto triplet = get_pnl(open_price, sell_price);
-//        auto sell_date = dates[idx + 1];
-//
-//        dic_records[tid]["close_date"] = sell_date;
-//        dic_records[tid]["close_price"] = std::to_string(sell_price);
-//        dic_records[tid]["holding_days"] = std::to_string(holding_days);
-//        dic_records[tid]["pnl"] = std::to_string(std::get<0>(triplet));
-//        dic_records[tid]["tax"] = std::to_string(std::get<1>(triplet));
-//        dic_records[tid]["fee"] = std::to_string(std::get<2>(triplet));
-//
-//        open_price = std::stof("NAN");
-//        holding_days = 0;
-//
-//        tid = available_tid;
-//        available_tid += 1;
-//        dic_records[tid] = {
-//                {"sid", sid},
-//                {"strategy_name", strategy_name},
-//                {"trader_code", trader_code},
-//                {"holding_days", "0"},
-//                {"last_check", "today"},
-//                {"open_price", "NAN"}
-//        };
-//      }
-//    } else {
-//      // try to buy
-//      if (selected[dates[idx]].count(sid)) {
-//        if (idx < o.size() - 1) {
-//          auto buy_date = dates[idx + 1];
-//          float buy_price = o[idx + 1];
-//          dic_records[tid]["open_date"] = buy_date;
-//          dic_records[tid]["open_price"] = std::to_string(buy_price);
-//          dic_records[tid]["long_short"] = "long";
-//          dic_records[tid]["shares"] = "1";
-//          open_price = buy_price;
-//          holding_days = 1;
-//        }
-//      }
-//    }
-//  }
-//  if(!std::isnan(open_price)) {
-//    auto triplet = get_pnl(open_price, c[c.size()-1]);
-//    dic_records[tid]["holding_days"] = std::to_string(holding_days);
-//    dic_records[tid]["pnl"] = std::to_string(std::get<0>(triplet));
-//  }
-//}
+void trade(std::map<int, std::map<std::string, std::string>> &dic_records,
+           int tid,
+           std::string &sid,
+           float open_price,
+           int holding_days,
+           int holding_days_th,
+           std::map<std::string, std::vector<std::string>> &last_date_signal,
+           int &available_tid,
+           std::vector<float> &o,
+           std::vector<float> &c,
+           std::vector<float> &ma20,
+           std::vector<std::string> &dates,
+           std::map<std::string, std::set<std::string>> &selected,
+           std::string &strategy_name,
+           std::string &trader_code) {
+
+  auto & last_date_sell_list = last_date_signal["sell"];
+
+  for (size_t idx = 0; idx < o.size(); ++idx) {
+    if (!std::isnan(open_price)) {
+      holding_days += (idx != o.size() - 1);
+      // try to sell
+      if (sell_logic(holding_days, holding_days_th, c[idx], ma20[idx])) {
+        if (idx == o.size() - 1) {
+          last_date_sell_list.push_back(sid);
+          break;
+        }
+
+        auto sell_price = o[idx + 1];
+        auto triplet = get_pnl(open_price, sell_price);
+        auto sell_date = dates[idx + 1];
+
+        dic_records[tid]["close_date"] = sell_date;
+        dic_records[tid]["close_price"] = std::to_string(sell_price);
+        dic_records[tid]["holding_days"] = std::to_string(holding_days);
+        dic_records[tid]["pnl"] = std::to_string(std::get<0>(triplet));
+        dic_records[tid]["tax"] = std::to_string(std::get<1>(triplet));
+        dic_records[tid]["fee"] = std::to_string(std::get<2>(triplet));
+
+        open_price = std::stof("NAN");
+        holding_days = 0;
+
+        tid = available_tid;
+        available_tid += 1;
+        dic_records[tid] = {
+                {"sid", sid},
+                {"strategy_name", strategy_name},
+                {"trader_code", trader_code},
+                {"holding_days", "0"},
+                {"last_check", "today"},
+                {"open_price", "NAN"}
+        };
+      }
+    } else {
+      // try to buy
+      if (selected[dates[idx]].count(sid)) {
+        if (idx < o.size() - 1) {
+          auto buy_date = dates[idx + 1];
+          float buy_price = o[idx + 1];
+          dic_records[tid]["open_date"] = buy_date;
+          dic_records[tid]["open_price"] = std::to_string(buy_price);
+          dic_records[tid]["long_short"] = "long";
+          dic_records[tid]["shares"] = "1";
+          open_price = buy_price;
+          holding_days = 1;
+        }
+      }
+    }
+  }
+  if(!std::isnan(open_price)) {
+    auto triplet = get_pnl(open_price, c[c.size()-1]);
+    dic_records[tid]["holding_days"] = std::to_string(holding_days);
+    dic_records[tid]["pnl"] = std::to_string(std::get<0>(triplet));
+  }
+}
 
 
 void trade_omp(std::map<int, std::map<std::string, std::string>> &dic_records,
@@ -137,13 +137,18 @@ void trade_omp(std::map<int, std::map<std::string, std::string>> &dic_records,
 
         // 為什麼這裡 assert 不會過？ 一定要 if
         if(auto dr = dic_records.find(tid); dr != dic_records.end()) {
-//        assert(dr != dic_records.end());
+          assert(dr != dic_records.end());
           dr->second.find("close_date")->second = sell_date;
           dr->second.find("close_price")->second = std::to_string(sell_price);
           dr->second.find("holding_days")->second = std::to_string(holding_days);
           dr->second.find("pnl")->second = std::to_string(std::get<0>(triplet));
           dr->second.find("tax")->second = std::to_string(std::get<1>(triplet));
           dr->second.find("fee")->second = std::to_string(std::get<2>(triplet));
+        } else {
+          for(int i=0; i<100; ++i) {
+            std::cout << "sell thread : " << omp_get_thread_num() << ", deal with " << sid
+                      << ", finding tid fail: " << tid << ", available: " << available_tid << std::endl;
+          }
         }
 
         open_price = std::stof("NAN");
@@ -166,6 +171,7 @@ void trade_omp(std::map<int, std::map<std::string, std::string>> &dic_records,
                 {"long_short",    "NAN"},
                 {"shares",        "NAN"}
         };
+
         tid = __sync_fetch_and_add(&available_tid, 1);
         #pragma omp critical
         {
@@ -179,14 +185,20 @@ void trade_omp(std::map<int, std::map<std::string, std::string>> &dic_records,
         if (idx < o.size() - 1) {
           auto buy_date = dates[idx + 1];
           float buy_price = o[idx + 1];
+          assert(!std::isnan(buy_price));
 
           // 為什麼這裡 assert 不會過？ 一定要 if
           if(auto dr = dic_records.find(tid); dr != dic_records.end()) {
-//          assert(dr != dic_records.end());
+            assert(dr != dic_records.end());
             dr->second.find("open_date")->second = buy_date;
             dr->second.find("open_price")->second = std::to_string(buy_price);
             dr->second.find("long_short")->second = "long";
             dr->second.find("shares")->second = "1";
+          } else {
+            for(int i=0; i<100; ++i) {
+              std::cout << "buy thread : " << omp_get_thread_num() << ", deal with " << sid
+                        << ", finding tid fail: " << tid << ", available: " << available_tid << std::endl;
+            }
           }
 
           open_price = buy_price;
@@ -200,61 +212,66 @@ void trade_omp(std::map<int, std::map<std::string, std::string>> &dic_records,
 
     // 為什麼這裡 assert 不會過？ 一定要 if
     if(auto dr = dic_records.find(tid); dr != dic_records.end()) {
-//    assert(dr != dic_records.end());
+      assert(dr != dic_records.end());
       dr->second.find("holding_days")->second = std::to_string(holding_days);
       dr->second.find("pnl")->second = std::to_string(std::get<0>(triplet));
+    } else {
+      for(int i=0; i<100; ++i) {
+        std::cout << "end thread : " << omp_get_thread_num() << ", deal with " << sid
+                  << ", finding tid fail: " << tid << ", available: " << available_tid << std::endl;
+      }
     }
   }
 }
 
 
 // TODO: parallelize this
-//std::map<int, std::map<std::string, std::string>> trade_on_sids(
-//        std::vector<std::string> sids,
-//        std::map<std::string, std::vector<float>> o,
-//        std::map<std::string, std::vector<float>> c,
-//        std::map<std::string, std::vector<float>> ma20,
-//        std::vector<std::string> dates,
-//        std::map<std::string, std::set<std::string>> selected,
-//        int holding_days_th,
-//        std::map<int, std::map<std::string, std::string>> dic_records,
-//        std::map<std::string, std::vector<std::string>> last_date_signal,
-//        std::map<std::string, int> sid2tid,
-//        int available_tid,
-//        std::string strategy_name,
-//        std::string trader_code) {
-//  for(std::vector<std::string>::iterator it=sids.begin(); it<sids.end(); ++it) {
-//    int tid;
-//    std::map<std::string, std::string> r;
-//    if(!sid2tid.count(*it)) {
-//      r = {
-//        {"sid", *it},
-//        {"strategy_name", strategy_name},
-//        {"trader_code", trader_code},
-//        {"holding_days", "0"},
-//        {"last_check", "today"},
-//        {"open_price", "NAN"}
-//      };
-//      tid = available_tid;
-//      available_tid += 1;
-//      dic_records[tid] = r;
-//    } else {
-//      tid = sid2tid[*it];
-//      dic_records[tid]["last_check"] = "today";
-//      r = dic_records[sid2tid[*it]];
-//    }
-//
-//    float open_price = std::stof(r["open_price"]);
-//    int holding_days = std::stoi(r["holding_days"]);
-//    trade(dic_records, tid, *it,
-//          open_price, holding_days, holding_days_th,
-//          last_date_signal, available_tid,
-//          o[*it], c[*it], ma20[*it], dates,
-//          selected, strategy_name, trader_code);
-//  }
-//
-//  return dic_records;
-//}
+std::map<int, std::map<std::string, std::string>> trade_on_sids(
+        std::vector<std::string> sids,
+        std::map<std::string, std::vector<float>> o,
+        std::map<std::string, std::vector<float>> c,
+        std::map<std::string, std::vector<float>> ma20,
+        std::vector<std::string> dates,
+        std::map<std::string, std::set<std::string>> selected,
+        int holding_days_th,
+        std::map<int, std::map<std::string, std::string>> dic_records,
+        std::map<std::string, std::vector<std::string>> last_date_signal,
+        std::map<std::string, int> sid2tid,
+        int available_tid,
+        std::string strategy_name,
+        std::string trader_code) {
+  for(auto it=sids.begin(); it<sids.end(); ++it) {
+    int tid;
+    std::map<std::string, std::string> r;
+    if(!sid2tid.count(*it)) {
+      r = {
+        {"sid", *it},
+        {"strategy_name", strategy_name},
+        {"trader_code", trader_code},
+        {"holding_days", "0"},
+        {"last_check", "today"},
+        {"open_price", "NAN"}
+      };
+      tid = available_tid;
+      available_tid += 1;
+      dic_records[tid] = r;
+    } else {
+      tid = sid2tid[*it];
+      dic_records[tid]["last_check"] = "today";
+      r = dic_records[sid2tid[*it]];
+    }
+
+    float open_price = std::stof(r["open_price"]);
+    int holding_days = std::stoi(r["holding_days"]);
+    trade(dic_records, tid, *it,
+          open_price, holding_days, holding_days_th,
+          last_date_signal, available_tid,
+          o[*it], c[*it], ma20[*it], dates,
+          selected, strategy_name, trader_code);
+  }
+
+  return dic_records;
+}
 
 
 std::map<int, std::map<std::string, std::string>> trade_on_sids_openmp(
@@ -299,7 +316,9 @@ std::map<int, std::map<std::string, std::string>> trade_on_sids_openmp(
               {"shares",        "NAN"}
       };
 
-      tid = __sync_fetch_and_add(&available_tid, 1);
+      #pragma omp atomic capture
+      tid = available_tid++;
+//      tid = __sync_fetch_and_add(&available_tid, 1);
       #pragma omp critical
       {
         dic_records.insert({tid, r});
@@ -315,7 +334,12 @@ std::map<int, std::map<std::string, std::string>> trade_on_sids_openmp(
     int holding_days = std::stoi(r.find("holding_days")->second);
 
     // 為什麼這裡 assert 不會過？
-    assert(dic_records.find(tid) != dic_records.end());
+    if(dic_records.find(tid) == dic_records.end()) {
+      for(int i=0; i<100; ++i) {
+        std::cout << "Outside thread : " << omp_get_thread_num() << ", deal with " << *it
+        << ", finding tid fail: " << tid << ", available: " << available_tid << std::endl;
+      }
+    }
     trade_omp(dic_records, tid, *it,
               open_price, holding_days, holding_days_th,
               last_date_signal, available_tid,
@@ -400,15 +424,14 @@ std::map<int, std::map<std::string, std::string>> trade_on_sids_openmp(
 //  std::cout << std::ctime(&time_tt) << std::endl;
 //}
 
-void openmp_hello() {
-  #pragma omp parallel
-  {
-    std::cout << "Hello from thread " << omp_get_thread_num() << std::endl;
-  }
-}
+//void openmp_hello() {
+//  #pragma omp parallel
+//  {
+//    std::cout << "Hello from thread " << omp_get_thread_num() << std::endl;
+//  }
+//}
 
 PYBIND11_MODULE(core, m) {
-//  m.def("trade_on_sids", &trade_on_sids);
+  m.def("trade_on_sids", &trade_on_sids);
   m.def("trade_on_sids_openmp", &trade_on_sids_openmp);
-  m.def("openmp_hello", &openmp_hello);
 }
